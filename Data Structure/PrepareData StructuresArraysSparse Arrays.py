bb8b7e3da4ddb6aa4a6c1909e1531c06df086e37
Ismael Sousa <165102646+ThisIsmael@users.noreply.github.com>
2025-08-24 11:50:15 -0300

    Create PrepareData StructuresArraysSparse Arrays.py

---
+++ Data Structure/PrepareData StructuresArraysSparse Arrays.py
@@ -0,0 +1,46 @@
+#!/bin/python3
+# Complete the 'matchingStrings' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts following parameters:
+#  1. STRING_ARRAY stringList
+#  2. STRING_ARRAY queries
+#
+
+def matchingStrings(stringList, queries):
+    list1 = stringList
+    result = []
+    for i in range (len(queries)):
+        result.append(0)
+        for j in range (len(list1)):
+            if list1[j] == queries[i]:
+                result[i] += 1
+    return result
+                
+        
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    stringList_count = int(input().strip())
+
+    stringList = []
+
+    for _ in range(stringList_count):
+        stringList_item = input()
+        stringList.append(stringList_item)
+
+    queries_count = int(input().strip())
+
+    queries = []
+
+    for _ in range(queries_count):
+        queries_item = input()
+        queries.append(queries_item)
+
+    res = matchingStrings(stringList, queries)
+
+    fptr.write('\n'.join(map(str, res)))
+    fptr.write('\n')
+
+    fptr.close()
